e43b65149a0281440b1b006466ad268995f27e4e
maria-aragao <mariafloraragao1@gmail.com>
2024-09-09 09:59:11 -0300

    Atividade de calcular a nota aritmédica

---
+++ Atividade 04.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+
+nota1 = int(input("Digite a primeira nota"))
+nota2 = int(input("Digite a segunda nota"))
+nota3 = int(input("Digite a terceira nota"))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"A sua {media}")
